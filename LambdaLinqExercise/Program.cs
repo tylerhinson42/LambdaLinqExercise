7a34d60fd99e92a39cf83ca38bc83e2adb0ad042
Tyler Hinson <tyler.hinson42@gmail.com>
2020-03-30 20:34:00 -0500

    1st commit

---
+++ LambdaLinqExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LambdaLinqExercise
+{
+    class Program
+    {
+        private static object animalNames;
+
+        static void Main(string[] args)
+        {
+            List<string> animalNames = new List<string>()
+
+            {"Fawn", "gibbon", "heron", "jackalope", "ibex" };
+            
+
+            IEnumerable<string> longANimalNames =
+                animalNames.OrderByDescending(x => x.Length);
+        }
+    }
+}
